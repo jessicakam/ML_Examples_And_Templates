553f5e4dbca440f18fd65318296096807dd874e6
Jessica_Kam <jessica.c.kam@berkeley.edu>
2017-07-29 11:26:59 -0700

    added basic outlines for classification

--- classification.py
+++ classification.py
@@ -4,25 +4,88 @@ Date: 2017/07/28
 """
 
 from data_preprocessing import DataPreprocessing
+from sklearn,metrics import confusion_matrix
+from matplotlib.colors import ListedColormap
 
-class LogisticRegression(DataPreprocessing):
-    pass
 
-class KNN(DataPreprocessing):
-    pass
+class Classification(DataPreprocessing, list_of_columns):
+    def __init__(self):
+        pass #
+        
+    def importDataset(self, csv_file):
+        self.dataset = pd.read_csv(csv_file)
+        self.X = dataset.iloc[: list_of_columns].values
+        self.y = dataset.iloc[:, y_index].values
+    
+    def scaleFeatures(self):
+        sc = StandardScaler()
+        self.X_train = sc.fit_transform(self.X_train)
+        self.X_test = sc.transform(self.X_test)
+        
+    def fitToTrainingSet(self):
+        pass
+    
+    def makeConfusionMatrix(self):
+       self.cm = confusion_matrix(self.y_test, self.y_pred) 
 
-class SVM(DataPreprocessing):
-    pass
+    def visualizeTrainingSetResults(self):
+        pass #
+    
+    def visualizeTestSetResults(self):
+        pass #
 
-class KernelSVM(DataPreprocessing):
-    pass
+from sklearn.linear_model import LogisticRegression
+class LogisticRegression(Classification):
+    def fitToTrainingSet(self, **kwargs):
+        self.classifier = LogisticRegression(**kwargs)
+        self.classifier.fit(self.X_train, self.y_train)
+    
+    def predictTestResults(self):
+        self.y_pred = self.classifier.predict(self.X_test)
+        
+    def visualizeTrainingSetResults(self):
+        pass #
+    
+    def visualizeTestSetResults(self):
+        pass #
 
-class NaiveBayes(DataPreprocessing):
-    pass
+form sklearn.neighbors import KNeighborsClassifier
+class KNN(Classification):
+    def fitToTrainingSet(self, **kwargs):
+        self.classifier = KNeighborsClassifier(**kwargs)
+        self.classifier.fit(self.X_train, self.y_train)
 
-class DecisionTreeClassification(DataPreprocessing):
-    pass
+    def visualizeTrain():
+        pass #
+    def visualizeTest():
+        pass #
+        
+from sklearn.svm import SVC
+class SVM(Classification):
+    def fitToTrainingSet(self, **kwargs):
+        self.classifier = SVC(**kwargs)
+        self.classifier.fit(self.X_train, self.y_train)
 
+
+class KernelSVM(SVM):
+    def fitToTrainingSet(self, **kwargs):
+        super(KernelSVM, self).fitToTrainingSet(kernel='rbf', **kwargs)
+
+from sklearn.naive_bayes import GaussianNB
+class NaiveBayes(Classification):
+    def fitToTrainingSet(self, **kwargs):
+        self.classifer = GaussianNB()
+        self.classifer.fit(self.X_train, self.y_train)
+
+from sklearn.tree import DecisionTreeClassifier
+class DecisionTreeClassification(Classification):
+    def fitToTrainingSet(self, **kwargs):
+        self.classifier = DecisionTreeClassifier(**kwargs)
+        self.classifier.fit(self.X_train, self.y_train)
+
+from sklearn.ensemble import RandomForestClassifier
 class RandomForestClassification(DecisionTreeClassification):
-    pass
+    def fitToTrainingSet(self, **kwargs):
+        self.classifier = RandomForestClassifier(**kwargs)
+        classifier.fit(self.X_train, self.y_train)
 
--- unittests/classification/classification_tests.py
+++ unittests/classification/classification_tests.py
@@ -2,7 +2,7 @@
 Name: Jessica Kam
 Date: 2017/07/28
 """
-import classification
+import classification as clf
 from unittest import TestCase
 import os,sys,inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -12,29 +12,83 @@ sys.path.insert(0,repo_dir)
 
 class TestClassificationClasses(TestCase):
     
-    def test_SimpleLinearRegression(self):
-        pass
-    
     def test_LogisticRegression(self):
-        pass
+        logistic_regression = clf.LogisticRegression()
+        logistic_regression.importDataset('Social_Network_Ads.csv', list_of_columns=[2,3], 4)
+        logistic_regression.splitIntoTrainingAndTestSets(test_size=0.25, random_state=0)
+        logistic_regression.scaleFeatures()
+        logistic_regression.fitToTrainingSet()
+        logistic_regression.predictTestResults()
+        logistic_regression.makeConfusionMatrix()
+        logistic_regression.visualizeTrainingsetResults() #
+        logistic_regression.visualizeTestSetResults() #
     
     def test_KNN(self):
-        pass
+        knn = clf.KNN()
+        knn.importDataset('Social_Network_Ads.csv', list_of_columns=[2,3], 4)
+        knn.splitIntoTrainingTestSet(test_size=0.25, random_state=0)
+        knn.scaleFeatures()
+        knn.fitToTrainingSet()
+        knn.predictTestResults()
+        knn.makeConfusionMatrix()
+        knn.visualizeTrain()#
+        knn.visualizeTest() #
     
     def test_SVM(self):
-        pass
+        svm = clf.SVM()
+        svm.importDataset('Social_Network_Ads.csv', list_of_columns=[2,3], 4)
+        svm.splitIntoTrainingTestSet(test_size=0.25, random_state=0)
+        svm.scaleFeatures()
+        svm.fitToTrainingSet()
+        svm.predictTestResults()
+        svm.makeConfusionMatrix()
+        svm.visualizeTrain()#
+        svm.visualizeTest() #
+        
     
     def test_KernelSVM(self):
-        pass
+        k_svm = clf.KernelSVM()
+        k_svm.importDataset('Social_Network_Ads.csv', list_of_columns=[2,3], 4)
+        k_svm.splitIntoTrainingTestSet(test_size=0.25, random_state=0)
+        k_svm.scaleFeatures()
+        k_svm.fitToTrainingSet(random_state=0)
+        k_svm.predictTestResults()
+        k_svm.makeConfusionMatrix()
+        k_svm.visualizeTrain()#
+        k_svm.visualizeTest() #
     
     def test_NaiveBayes(self):
-        pass
+        k_svm = clf.KernelSVM()
+        k_svm.importDataset('Social_Network_Ads.csv', list_of_columns=[2,3], 4)
+        k_svm.splitIntoTrainingTestSet(test_size=0.25, random_state=0)
+        k_svm.scaleFeatures()
+        k_svm.fitToTrainingSet(random_state=0)
+        k_svm.predictTestResults()
+        k_svm.makeConfusionMatrix()
+        k_svm.visualizeTrain()#
+        k_svm.visualizeTest() #
     
     def test_DecisionTreeClassification(self):
-        pass
+        decision_tree = clf.DecisionTreeClassification()
+        decision_tree.importDataset('Social_Network_Ads.csv', list_of_columns=[2,3], 4)
+        decision_tree.splitIntoTrainingTestSet(test_size=0.25, random_state=0)
+        decision_tree.scaleFeatures()
+        decision_tree.fitToTrainingSet(criterion=entropy, random_state=0)
+        decision_tree.predictTestResults()
+        decision_tree.makeConfusionMatrix()
+        decision_tree.visualizeTrain()#
+        decision_tree.visualizeTest() #
     
     def test_RandomForestClassification(self):
-        pass
+        rand_forest = clf.RandomForestClassification()
+        rand_forest.importDataset('Social_Network_Ads.csv', list_of_columns=[2,3], 4)
+        rand_forest.splitIntoTrainingTestSet(test_size=0.25, random_state=0)
+        rand_forest.scaleFeatures()
+        rand_forest.fitToTrainingSet(n_estimators=10, criterion=10, random_state=0)
+        rand_forest.predictTestResults()
+        rand_forest.makeConfusionMatrix()
+        rand_forest.visualizeTrain()#
+        rand_forest.visualizeTest() #
 
 if __name__ == '__main__':
     unittest.main()
