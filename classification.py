c6193d70257f44db75af470857df7e7771943e6c
Jessica_Kam <jessica.c.kam@berkeley.edu>
2017-08-01 23:36:50 -0700

    tests for classification added but not yet pass

--- classification.py
+++ classification.py
@@ -1,35 +1,26 @@
-"""
-Name: Jessica Kam
-Date: 2017/07/28
-"""
+# 2017/07/28
 
-from data_preprocessing import DataPreprocessing
-from sklearn.metrics import confusion_matrix
+from data_preprocessing import DataPreProcessing
+from data_postprocessing import DataPostProcessing
 from matplotlib.colors import ListedColormap
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-class Classification(DataPreprocessing):
+class Classification(DataPreProcessing, DataPostProcessing):
     def __init__(self):
-        pass #
-        
-    def importDataset(self, csv_file, list_of_columns):
-        self.dataset = pd.read_csv(csv_file)
-        self.X = dataset.iloc[: list_of_columns].values
-        self.y = dataset.iloc[:, y_index].values
-    
-    def scaleFeatures(self):
-        sc = StandardScaler()
-        self.X_train = sc.fit_transform(self.X_train)
-        self.X_test = sc.transform(self.X_test)
-        
+        super(Classification, self).__init__()
+        self.name = None
+        self.classifier = None
+   
     def fitToTrainingSet(self):
         pass
     
-    def makeConfusionMatrix(self):
-       self.cm = confusion_matrix(self.y_test, self.y_pred) 
-   
+    def predictTestResults(self):
+        self.y_pred = self.classifier.predict(self.X_test)
+    
     def visualizeResults(self, color1, color2, what_visualizing, xlabel, ylabel):
-        X_set, y_set = X_train, y_train
+        X_set, y_set = self.X_train, self.y_train
         X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                              np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
         plt.contourf(X1, X2, self.classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
@@ -49,35 +40,38 @@ class Classification(DataPreprocessing):
         return self.name + ' ' + '(' + what_visualizing + ')'
     
     def visualizeTrainingSetResults(self, color1, color2, xlabel, ylabel):
-        self.visualizeResults(color1, color2, what_visualizing='Training Set', xlabel, ylabel)
+        self.visualizeResults(color1, color2, 'Training Set', xlabel, ylabel)
      
     def visualizeTestSetResults(self, color1, color2, xlabel, ylabel):
-        self.visualizeResults(=color1, color2, what_visualizing='Test Set', xlabel, ylabel)
+        self.visualizeResults(color1, color2, 'Test Set', xlabel, ylabel)
 
-from sklearn.linear_model import LogisticRegression
-class LogisticRegression(Classification):
-    def __init__(self):
-        self.name = 'LogisticRegression'
         
-    def fitToTrainingSet(self, **kwargs):
-        self.classifier = LogisticRegression(**kwargs)
+from sklearn.linear_model import LogisticRegression
+class LogisticReg(Classification):
+    def __init__(self):
+        super(LogisticReg, self).__init__()
+        self.name = 'LogisticReg'
+        
+    def fitToTrainingSet(self):
+        self.classifier = LogisticRegression()
         self.classifier.fit(self.X_train, self.y_train)
     
-    def predictTestResults(self):
-        self.y_pred = self.classifier.predict(self.X_test)
-
-form sklearn.neighbors import KNeighborsClassifier
+        
+from sklearn.neighbors import KNeighborsClassifier
 class KNN(Classification):
     def __init__(self):
+        super(KNN, self).__init__()
         self.name = 'KNN'
         
     def fitToTrainingSet(self, **kwargs):
         self.classifier = KNeighborsClassifier(**kwargs)
         self.classifier.fit(self.X_train, self.y_train)
+
         
 from sklearn.svm import SVC
 class SVM(Classification):
     def __init__(self):
+        super(SVM, self).__init__()
         self.name = 'SVM'
         
     def fitToTrainingSet(self, **kwargs):
@@ -87,34 +81,41 @@ class SVM(Classification):
 
 class KernelSVM(SVM):
     def __init__(self):
+        super(KernelSVM, self).__init__()
         self.name = 'KernelSVM'
         
     def fitToTrainingSet(self, **kwargs):
         super(KernelSVM, self).fitToTrainingSet(kernel='rbf', **kwargs)
 
+        
 from sklearn.naive_bayes import GaussianNB
 class NaiveBayes(Classification):
     def __init__(self):
+        super(NaiveBayes, self).__init__()
         self.name = 'NaiveBayes'
         
     def fitToTrainingSet(self, **kwargs):
         self.classifer = GaussianNB()
         self.classifer.fit(self.X_train, self.y_train)
 
+        
 from sklearn.tree import DecisionTreeClassifier
 class DecisionTreeClassification(Classification):
     def __init__(self):
-        self.name = 'DecisonTreeClassif'
+        super(DecisionTreeClassification, self).__init__()
+        self.name = 'DecisionTreeClassification'
     
     def fitToTrainingSet(self, **kwargs):
         self.classifier = DecisionTreeClassifier(**kwargs)
         self.classifier.fit(self.X_train, self.y_train)
 
+        
 from sklearn.ensemble import RandomForestClassifier
 class RandomForestClassification(DecisionTreeClassification):
-    def __init__(self);:
-        self.name = 'RandomForestClassif'
+    def __init__(self):
+        super(RandomForestClassification, self).__init__()
+        self.name = 'RandomForestClassification'
         
     def fitToTrainingSet(self, **kwargs):
         self.classifier = RandomForestClassifier(**kwargs)
-        classifier.fit(self.X_train, self.y_train)
+        self.classifier.fit(self.X_train, self.y_train)
--- unittests/classification/classification_tests.py
+++ unittests/classification/classification_tests.py
@@ -1,33 +1,66 @@
-"""
-Name: Jessica Kam
-Date: 2017/07/28
-"""
-import classification as clf
+# 2017/07/28
+
+import unittest
 from unittest import TestCase
 import os,sys,inspect
+
+# To get correct directory for file being tested
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 repo_dir = os.path.dirname(parentdir)
-sys.path.insert(0,repo_dir) 
+sys.path.insert(0,repo_dir)
+import classification as clf
 
-class TestClassificationClasses(TestCase):
-    
-    def test_LogisticRegression(self):
-        logistic_regression = clf.LogisticRegression()
-        logistic_regression.importDataset('Social_Network_Ads.csv', list_of_columns=[2,3], 4)
+
+class TestClassificationClasses(TestCase):        
+    def test_LogisticReg(self):
+        logistic_regression = clf.LogisticReg()
+        instance = 'logistic_regression'
+        self.assertEqual(logistic_regression.X, None, instance + '.X should be None.')
+        self.assertEqual(logistic_regression.y, None, instance + '.y should be None.')
+        self.assertEqual(logistic_regression.name, 'LogisticReg', instance + '.name is incorrect.')
+        
+        logistic_regression.importDataset2('Social_Network_Ads.csv', [2,3], 4)
+        self.assertTrue(logistic_regression.X.all(), instance + '.X should be set after importing dataset')
+        self.assertTrue(logistic_regression.y.all(), instance + '.y should be set after importing dataset.')
+        print("X: {0} \n".format(logistic_regression.X))
+        print("y: {0} \n".format(logistic_regression.y))
+        
         logistic_regression.splitIntoTrainingAndTestSets(test_size=0.25, random_state=0)
-        logistic_regression.scaleFeatures()
+        self.assertTrue(logistic_regression.X_train, instance + '.X_train should be set after importing dataset')
+        self.assertTrue(logistic_regression.X_test, instance + 'X_test should be set after importing dataset.')
+        self.assertTrue(logistic_regression.y_train, instance + '.y_train should be set after importing dataset')
+        self.assertTrue(logistic_regression.y_test, instance + '.y_test should be set after importing dataset.')
+        print("X_train: {0} \n".format(logistic_regression.X_train))
+        print("X_test: {0} \n".format(logistic_regression.X_test))
+        print("y_train: {0} \n".format(logistic_regression.y_train))
+        print("y_test: {0} \n".format(logistic_regression.y_test))
+        
+        logistic_regression.scaleFeatures2()
+        self.assertTrue(logistic_regression.sc, instance + '.sc has not been set. X_train and X_test has not been scaled.')
+        
         logistic_regression.fitToTrainingSet()
+        self.assertTrue(logistic_regression.classifier, instance + '.classifier has not been set. Training set not fitted.')
+        
         logistic_regression.predictTestResults()
+        self.assertTrue(logistic_regression.y_pred, instance + '.y_pred has not been set. Results not predicted.')
+        print("y_pred: {0} \n".format(logistic_regression.y_pred))
+        
         logistic_regression.makeConfusionMatrix()
+        self.assertTrue(self.cm, instance + '.cm not set. Confusion matrix not created.')
+        print("cm: {0}".format(logistic_regression.cm))
+        
         logistic_regression.visualizeTrainingsetResults('red', 'green', 'Age', 'Estimated Salary')
         logistic_regression.visualizeTestSetResults('red', 'green', 'Age', 'Estimated Salary')
     
     def test_KNN(self):
         knn = clf.KNN()
-        knn.importDataset('Social_Network_Ads.csv', list_of_columns=[2,3], 4)
+        instance = 'knn'
+        self.assertEqual(knn.name, 'KNN', instance + '.name is incorrect.')
+        
+        knn.importDataset2('Social_Network_Ads.csv', [2,3], 4)        
         knn.splitIntoTrainingAndTestSets(test_size=0.25, random_state=0)
-        knn.scaleFeatures()
+        knn.scaleFeatures2()
         knn.fitToTrainingSet()
         knn.predictTestResults()
         knn.makeConfusionMatrix()
@@ -36,21 +69,26 @@ class TestClassificationClasses(TestCase):
     
     def test_SVM(self):
         svm = clf.SVM()
-        svm.importDataset('Social_Network_Ads.csv', list_of_columns=[2,3], 4)
+        instance = 'svm'
+        self.assertEqual(svm.name, 'SVM', instance + '.name is incorrect.')
+        
+        svm.importDataset2('Social_Network_Ads.csv', [2,3], 4)        
         svm.splitIntoTrainingAndTestSets(test_size=0.25, random_state=0)
-        svm.scaleFeatures()
+        svm.scaleFeatures2()
         svm.fitToTrainingSet()
         svm.predictTestResults()
         svm.makeConfusionMatrix()
         svm.visualizeTrain('red', 'green', 'Age', 'Estimated Salary')
         svm.visualizeTest('red', 'green', 'Age', 'Estimated Salary')
         
-    
     def test_KernelSVM(self):
         k_svm = clf.KernelSVM()
-        k_svm.importDataset('Social_Network_Ads.csv', list_of_columns=[2,3], 4)
+        instance = 'k_svm'
+        self.assertEqual(k_svm.name, 'KernelSVM', instance + '.name is incorrect.')
+        
+        k_svm.importDataset2('Social_Network_Ads.csv', [2,3], 4)
         k_svm.splitIntoTrainingAndTestSets(test_size=0.25, random_state=0)
-        k_svm.scaleFeatures()
+        k_svm.scaleFeatures2()
         k_svm.fitToTrainingSet(random_state=0)
         k_svm.predictTestResults()
         k_svm.makeConfusionMatrix()
@@ -58,22 +96,28 @@ class TestClassificationClasses(TestCase):
         k_svm.visualizeTest('red', 'green', 'Age', 'Estimated Salary')
     
     def test_NaiveBayes(self):
-        k_svm = clf.KernelSVM()
-        k_svm.importDataset('Social_Network_Ads.csv', list_of_columns=[2,3], 4)
-        k_svm.splitIntoTrainingAndTestSets(test_size=0.25, random_state=0)
-        k_svm.scaleFeatures()
-        k_svm.fitToTrainingSet(random_state=0)
-        k_svm.predictTestResults()
-        k_svm.makeConfusionMatrix()
-        k_svm.visualizeTrain('red', 'green', 'Age', 'Estimated Salary')
-        k_svm.visualizeTest('red', 'green', 'Age', 'Estimated Salary')
+        nb = clf.NaiveBayes()
+        instance = 'nb'
+        self.assertEqual(nb.name, 'NaiveBayes', instance + '.name is incorrect.')
+        
+        nb.importDataset2('Social_Network_Ads.csv', [2,3], 4)
+        nb.splitIntoTrainingAndTestSets(test_size=0.25, random_state=0)
+        nb.scaleFeatures2()
+        nb.fitToTrainingSet(random_state=0)
+        nb.predictTestResults()
+        nb.makeConfusionMatrix()
+        nb.visualizeTrain('red', 'green', 'Age', 'Estimated Salary')
+        nb.visualizeTest('red', 'green', 'Age', 'Estimated Salary')
     
     def test_DecisionTreeClassification(self):
         decision_tree = clf.DecisionTreeClassification()
-        decision_tree.importDataset('Social_Network_Ads.csv', list_of_columns=[2,3], 4)
+        instance = 'decision_tree'
+        self.assertEqual(decision_tree.name, 'DecisionTreeClassification', instance + '.name is incorrect.')
+        decision_tree.importDataset2('Social_Network_Ads.csv', [2,3], 4)
+        
         decision_tree.splitIntoTrainingAndTestSets(test_size=0.25, random_state=0)
-        decision_tree.scaleFeatures()
-        decision_tree.fitToTrainingSet(criterion=entropy, random_state=0)
+        decision_tree.scaleFeatures2()
+        decision_tree.fitToTrainingSet(criterion='entropy', random_state=0)
         decision_tree.predictTestResults()
         decision_tree.makeConfusionMatrix()
         decision_tree.visualizeTrain('red', 'green', 'Age', 'Estimated Salary')
@@ -81,7 +125,10 @@ class TestClassificationClasses(TestCase):
     
     def test_RandomForestClassification(self):
         rand_forest = clf.RandomForestClassification()
-        rand_forest.importDataset('Social_Network_Ads.csv', list_of_columns=[2,3], 4)
+        instance = 'rand_forest'
+        self.assertEqual(rand_forest.name, 'RandomForestClassification', instance + '.name is incorrect.')
+        
+        rand_forest.importDataset2('Social_Network_Ads.csv', [2,3], 4)
         rand_forest.splitIntoTrainingAndTestSets(test_size=0.25, random_state=0)
         rand_forest.scaleFeatures()
         rand_forest.fitToTrainingSet(n_estimators=10, criterion=10, random_state=0)
@@ -90,5 +137,6 @@ class TestClassificationClasses(TestCase):
         rand_forest.visualizeTrain('red', 'green', 'Age', 'Estimated Salary')
         rand_forest.visualizeTest('red', 'green', 'Age', 'Estimated Salary')
 
+        
 if __name__ == '__main__':
     unittest.main()
