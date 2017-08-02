1a9fddf82be27711614ddfd4ae0224d40981172b
Jessica_Kam <jessica.c.kam@berkeley.edu>
2017-08-01 22:02:17 -0700

    assoc rule learning tests pass

--- association_rule_learning.py
+++ association_rule_learning.py
@@ -1,29 +1,27 @@
-"""
-2017/07/29
-"""
+# 2017/07/29
 
-from data_preprocessing import DataPreprocessing
+from data_preprocessing import DataPreProcessing
+import pandas as pd
 
-class AssociationRuleLearning(DataPreprocessing):
-    pass
-
+class AssociationRuleLearning(DataPreProcessing):
+    def __init__(self):
+        super(AssociationRuleLearning, self).__init__()
+        
 from apyori import apriori
 class Apriori(AssociationRuleLearning):
     def __init__(self):
-        pass
-    
-    def preprocessData(self, csv_file, header, num_observations, range_max):
-        dataset = pd.read_csv(csv_file, header)
+        super(Apriori, self).__init__()
+        
+    def preprocessData(self, csv_file, header, num_lines, range_max):
+        self.dataset = pd.read_csv(csv_file, header, engine='python')
         self.transactions = []
-        for i in range(0, num_observations):
-            self.transactions.append([str(dataset.values[i,j]) for j in range(0, range_max)])
+        for i in range(0, num_lines):
+            print(str(i) + '\n') #
+            self.transactions.append([str(self.dataset.values[i,j]) for j in range(0, range_max)])
     
     def trainOnDataset(self, **kwargs):
         self.rules = apriori(self.transactions, **kwargs)
     
     def visualizeResults(self):
         self.results = list(self.rules)
-
-#later
-#class Eiat(DataPreprocessing): #correct spelling?
-#    pass
+        return self.results
--- unittests/association_rule_learning/association_tests.py
+++ unittests/association_rule_learning/association_tests.py
@@ -1,18 +1,33 @@
-"""
-Date: 2017/07/29
-"""
-import association_rule_learning as assoc_rule_learn
+# 2017/07/29
+
+import unittest
 from unittest import TestCase
 import os,sys,inspect
+
+# To get correct directory for file being tested
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 repo_dir = os.path.dirname(parentdir)
 sys.path.insert(0,repo_dir) 
+import association_rule_learning as assoc_rule_learn
+
 
 class TestAssociationRuleLearningClasses(TestCase):
-    
     def test_Apriori(self):
         apriori = assoc_rule_learn.Apriori()
-        apriori.preprocessData('Market_Basket_Optimization.csv', header=None, num_observations=7501, range_max=20)
+        self.assertEqual(apriori.X, None, 'apriori.X should be None.')
+        self.assertEqual(apriori.y, None, 'apriori.y should be None.')
+        print("got past assertEquals") #
+        apriori.preprocessData('Market_Basket_Optimisation.csv', header=None, num_lines=7500, range_max=20,)
+        self.assertFalse(apriori.dataset.empty, 'apriori.dataset should not be empty.')
+        print("dataset: {0}".format(apriori.dataset)) #
         apriori.trainOnDataset(min_support=0.003, min_confidence=0.2, min_lift=3, min_length=2)
-        apriori.visualizeResults()
+        self.assertTrue(apriori.rules, 'apriori should have a rules attribute')
+        print("rules: {0}".format(apriori.rules)) #
+        results = apriori.visualizeResults()
+        self.assertTrue(results, 'apriori should return results (a list of rules).')
+        print("results: {0}".format(apriori.results)) #
+ 
+        
+if __name__ == '__main__':
+    unittest.main()
