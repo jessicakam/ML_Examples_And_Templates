f4de7d7816e01b4249faf910611e598f9a236c22
Jessica_Kam <jessica.c.kam@berkeley.edu>
2017-08-02 20:09:24 -0700

    clustering tests now pass

--- clustering.py
+++ clustering.py
@@ -1,20 +1,16 @@
-"""
-Name: Jessica Kam
-Date: 2017/07/29
-"""
-from data_preprocessing import DataPreprocessing
+# 2017/07/29
 
-class Cluster(DataPreprocessing):
+from data_preprocessing import DataPreProcessing
+from data_postprocessing import DataPostProcessing
+import matplotlib.pyplot as plt
+
+class Cluster(DataPreProcessing, DataPostProcessing):
     def __init__(self):
-        pass
-    
-    def importDataset(self, csv_file, list_of_columns):
-        dataset = pd.read_csv(csv_file)
-        self.X = dataset.iloc[:, list_of_columns].values
+        super(Cluster, self).__init__()
         
     def visualizeClusters(self, num_clusters, lst_colors, title, xlabel, ylabel):
-        for i in range num_clusters:
-            plt.scatter(self.X[self.y_cluster == i, 0], self.X[self.y_cluster == i, 1], s=100, c=lst_colors[i], label='Cluster'+i+1)
+        for i in range(num_clusters):
+            plt.scatter(self.X[self.y_cluster == i, 0], self.X[self.y_cluster == i, 1], s=100, c=lst_colors[i], label='Cluster'+str(i+1))
         if 'KMeans' in self.name:
             plt.scatter(self.kmeans.cluster_centers_[:, 0], self.kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
         plt.title(title)
@@ -24,20 +20,21 @@ class Cluster(DataPreprocessing):
         plt.show()
         
 from sklearn.cluster import KMeans
-class KMeans(Cluster):
+class KMeansClustering(Cluster):
     def __init__(self):
-        self.name = 'KMeans'
+        super(KMeansClustering, self).__init__()
+        self.name = 'KMeansClustering'
         
-    def findOptimalNumberOfClusters(self, max_num_clusters=11, **kwargs):
+    def findOptimalNumberOfClusters(self, xlabel, ylabel, max_num_clusters, **kwargs):
         wcss = []
         for i in range(1, max_num_clusters):
             kmeans = KMeans(n_clusters = i, **kwargs)
-            kmeans.fit(X)
+            kmeans.fit(self.X)
             wcss.append(kmeans.inertia_)
         plt.plot(range(1, max_num_clusters), wcss)
         plt.title('Opt Num Clusters Using the Elbow Method')
-        plt.xlabel('Number of clusters')
-        plt.ylabel('WCSS')
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
         plt.show()
 
     def fitToDataset(self, **kwargs):
@@ -47,11 +44,15 @@ class KMeans(Cluster):
 import scipy.cluster.hierarchy as sch
 from sklearn.cluster import AgglomerativeClustering
 class HierarchialClustering(Cluster):
-    def findOptimalNumberOfClusters(self, **kwargs):
-        dendrogram = sch.dendrogram(sch.linkage(self.X, **kwargs))
+    def __init__(self):
+        super(HierarchialClustering, self).__init__()
+        self.name = 'HierarchialClustering'
+        
+    def findOptimalNumberOfClusters(self, xlabel, ylabel, **kwargs):
+        self.dendrogram = sch.dendrogram(sch.linkage(self.X, **kwargs))
         plt.title('Opt Num Clusters Using Dendrogram')
-        plt.xlabel('Customers')
-        plt.ylabel('Euclidean distances')
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
         plt.show()
         
     def fitToDataset(self, **kwargs):
--- unittests/clustering/clustering_tests.py
+++ unittests/clustering/clustering_tests.py
@@ -1,29 +1,47 @@
-"""
-Date: 2017/07/29
-"""
-import clustering as cluster
+# 2017/07/29
+
+import unittest
 from unittest import TestCase
 import os,sys,inspect
+
+# To get correct directory for file being tested
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 repo_dir = os.path.dirname(parentdir)
-sys.path.insert(0,repo_dir) 
+sys.path.insert(0,repo_dir)
+import clustering as cluster
 
 class TestClusteringClasses(TestCase):
     
-    def test_KMeans(self):
-        kmeans = cluster.KMeans()
-        kmeans.importDataset('Mail_Customers.csv', list_of_columns=[3, 4])
-        kmeans.findOptimalNumberOfClusters(max_num_clusters=11, init='k-means++', random_state=42)
+    def test_KMeansClustering(self):
+        kmeans = cluster.KMeansClustering()
+        instance = 'kmeans'
+        
+        kmeans.importDataset3('Mall_Customers.csv', [3, 4])
+        self.assertTrue(kmeans.X.all(), instance + '.X should be set after importing dataset')
+        
+        kmeans.findOptimalNumberOfClusters(xlabel='Num of clusters', ylabel='WCSS', max_num_clusters=11, init='k-means++', random_state=42)
+        
         kmeans.fitToDataset(n_clusters=5, init='k-means++', random_state=42)
-        kmeans.visualizeResults(num_clusters=5, lst_colors=['red', 'blue', 'green', 'cyan', 'magenta'], 'Clusters of customers', 'Annual Income (k$)', 'Spending Score (1-100)') #
+        self.assertTrue(kmeans.kmeans, instance + '.kmeans has not been set.')
+        self.assertTrue(kmeans.y_cluster.any(), instance + '.y_cluster has not been set. Data not fitted.')
+        
+        kmeans.visualizeClusters(num_clusters=5, lst_colors=['red', 'blue', 'green', 'cyan', 'magenta'], title='Clusters of customers', xlabel='Annual Income (k$)', ylabel='Spending Score (1-100)')
         
     def test_HierarchialClustering(self):
         hierarchial_clust = cluster.HierarchialClustering()
-        hierarchial_clust.importDataset('Mail_Customers.csv', list_of_columns=[3, 4])
-        hierarchial_clust.findOptimalNumberOfClusters(method='ward')
+        instance = 'hierarchial_clust'
+        
+        hierarchial_clust.importDataset3('Mall_Customers.csv', [3, 4])
+        self.assertTrue(hierarchial_clust.X.all(), instance + '.X should be set after importing dataset')
+        
+        hierarchial_clust.findOptimalNumberOfClusters(xlabel='Customers', ylabel='Euclidean distances', method='ward')
+        
         hierarchial_clust.fitToDataset(n_clusters=5, affinity='euclidean', linkage='ward')
-        hierarchial_clust.visualizeResults(num_clusters=5, lst_colors=['red', 'blue', 'green', 'cyan', 'magenta'], 'Clusters of customers', 'Annual Income (k$)', 'Spending Score (1-100)') #
+        self.assertTrue(hierarchial_clust.hc, instance + '.hc has not been set.')
+        self.assertTrue(hierarchial_clust.y_cluster.any(), instance + '.y_cluster has not been set. Data not fitted.')
+        
+        hierarchial_clust.visualizeClusters(num_clusters=5, lst_colors=['red', 'blue', 'green', 'cyan', 'magenta'], title='Clusters of customers', xlabel='Annual Income (k$)', ylabel='Spending Score (1-100)')
         
     
     
