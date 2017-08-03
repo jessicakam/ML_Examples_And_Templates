a3525f2e9ead349918e59c546db20462d1428ee8
Jessica_Kam <jessica.c.kam@berkeley.edu>
2017-08-02 22:42:49 -0700

    nlp test passes

--- nlp.py
+++ nlp.py
@@ -1,23 +1,23 @@
-#Date: 2017/07/29
+# 2017/07/29
 
-from data_preprocessing import DataPreprocessing
+from data_preprocessing import DataPreProcessing
+from data_postprocessing import DataPostProcessing
 import re
 import nltk
+import pandas as pd
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import confusion_matrix
-class NLP(DataPreprocessing):
-    def importDataset(self, file, delimiter, quoting):
-        self.dataset = pd.read_csv(file, delimiter, quoting)
+
+class NLP(DataPreProcessing, DataPostProcessing):
+    def importDataset(self, file, **kwargs):
+        self.dataset = pd.read_csv(file, **kwargs)
         
-    def cleanTexts(self):
-        #
+    def cleanTexts(self, max_range):
         nltk.download('stopwords')
         self.corpus = []
-        for i in range(0, 1000):
+        for i in range(0, max_range):
             review = re.sub('[^a-zA-Z]', ' ', self.dataset['Review'][i])
             review = review.lower()
             review = review.split()
@@ -26,19 +26,12 @@ class NLP(DataPreprocessing):
             review = ' '.join(review)
             self.corpus.append(review)
 
-    def createBagOfWordsModel(self):
-        #
-        cv = CountVectorizer(max_features = 1500)
+    def createBagOfWordsModel(self, **kwargs):
+        cv = CountVectorizer(**kwargs)
         self.X = cv.fit_transform(self.corpus).toarray()
         self.y = self.dataset.iloc[:, 1].values
     
     def fitNaiveBayesToTrainingSet(self):
         self.classifier = GaussianNB()
         self.classifier.fit(self.X_train, self.y_train)
-        
-    def predictResults(self):
-        #
-        self.y_pred = self.classifier.predict(self.X_test)
-    
-    def makeConfusionMatrix(self):
-        self.cm = confusion_matrix(y_test, y_pred)
+
--- unittests/nlp/nlp_tests.py
+++ unittests/nlp/nlp_tests.py
@@ -1,20 +1,44 @@
-#Date: 2017/07/29
+# 2017/07/29
 
-import nlp
+import unittest
 from unittest import TestCase
 import os,sys,inspect
+
+# To get correct directory for file being tested
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 repo_dir = os.path.dirname(parentdir)
-sys.path.insert(0,repo_dir) 
+sys.path.insert(0,repo_dir)
+import nlp
+
 
 class TestNLPClasses(TestCase):
     def test_NLP(self):
-        nlp = nlp.NLP()
-        nlp.importDataset('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
-        nlp.cleanTexts()
-        nlp.createBagOfWordsModel()
-        nlp.splitIntoTrainingAndTestSets(test_size=0.2, random_state=0)
-        nlp.fitNaiveBayesToTrainingSet()
-        nlp.predictResults()
-        nlp.makeConfusionMatrix()
+        nlp_inst = nlp.NLP()
+        instance = 'nlp'
+        
+        nlp_inst.importDataset('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+        #self.assertTrue(nlp_inst.dataset.all(), instance + '.dataset should be set.')
+        
+        nlp_inst.cleanTexts(max_range=1000)
+        self.assertTrue(nlp_inst.corpus, instance + '.corpus should not be empty.')
+        
+        nlp_inst.createBagOfWordsModel(max_features = 1500)
+        self.assertTrue(nlp_inst.X.any(), instance + '.X should be set.')
+        self.assertTrue(nlp_inst.y.any(), instance + '.y should be set.')
+        
+        nlp_inst.splitIntoTrainingAndTestSets(test_size=0.2, random_state=0)
+        
+        nlp_inst.fitNaiveBayesToTrainingSet()
+        self.assertTrue(nlp_inst.classifier, instance + '.classifier should be set.')
+        
+        
+        nlp_inst.predictResults()
+        self.assertTrue(nlp_inst.y_pred.any(), instance + '.y_pred should be set.')
+        
+        nlp_inst.makeConfusionMatrix()
+        self.assertTrue(nlp_inst.cm.all(), instance + '.cm should be created.')
+        
+        
+if __name__ == '__main__':
+    unittest.main()
